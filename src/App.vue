14f8d76c463e65e90ed802f9886c93e13d76cdd0
vidalCreates <amc12vidal@hotmail.com>
2018-10-15 13:56:05 -0500

    Initialize project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Landing />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
 
 <style>
+
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  overflow: auto;
 }
+
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="landing">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>My SPA Template</h1>
+    <h2>Using VueJS</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing'
+}
+</script>
+
+<style scoped>
+
+.landing {
+  background: #4388a5;
+  min-width: 100vw;
+  min-height: 100vh;
+  color: white;
+}
+
+</style>
